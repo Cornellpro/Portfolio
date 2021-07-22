32e985bd18ee24c5286b068ad40a0179dabddf04
cornell-ui <74830571+cornell-ui@users.noreply.github.com>
2021-07-22 15:46:28 +0100

    paged

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-$(document).ready(function () {
-
-  $(document).on('click', '.custom-toggle', function (e) {
-      e.preventDefault();
-
-
-      $('.custom-body').hide();
-      
-      var click_body = $(this).attr('data-board');
-      
-      $(click_body).show();
-
-  });
-});
-
 // show Nav-Menu for smaller screen
 function navMenu() {
     document.getElementById("navMenu").classList.toggle("show-nav");
